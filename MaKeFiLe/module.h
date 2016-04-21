3ada426a51a7f602f0ba8273d87adcfd394f8654
Anjali Pardeshi <anjalipardeshi92@gmail.com>
2016-04-21 10:10:59 +0530

    simple makefile

---
+++ MaKeFiLe/module.h
@@ -0,0 +1,14 @@
+/*
+Info:
+	Make file should be named makefile or Makefile or GNUMakefile
+	To run default target use command  "make"
+	clean not getting called by default.
+	"make clean" command for forcefull calling.
+	Indentation important tabs used.
+	Editor should insert tabs not spaces.
+*/
+
+#include<stdio.h>
+
+void sample_func();
+
